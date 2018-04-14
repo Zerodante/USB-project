4d8d596ff110cf2d8a51731cae6eb16a6336c934
daniel hayabusa <dsanchen@outlook.es>
2018-04-13 19:27:10 -0500

    index change1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<header> 
+      <a href="#inicio" class="logo" data-scroll=""> <img src="resourcesGIN/imgGIN/inicio/logo-GINxti.png" height="60px" style="margin-top:10px; margin-right:20px"> </a>
+      <nav class="nav-collapse" style="padding-top:7px">
+</header>
+<body>
+    
+</body>
+</html>
