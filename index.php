183de6acf1400301a9df7855489ccee1488942a5
daniel hayabusa <dsanchen@outlook.es>
2018-04-13 22:03:00 -0500

    Index

--- index.php
+++ index.php
@@ -1,16 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="Sistema de Gestión Universitaria Luna">
+	<title>Universidad Simón Bolívar / Profesores</title>
+	<link rel="icon" href="favicon.ico" type="image/x-icon" >
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" >
+	<link rel="stylesheet" href="css/bootstrap.min.css"  media="screen">
+	<link rel="stylesheet" href="css/facebox.css" type="text/css" media="screen" >
+	<link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/usb.css">
+	<link rel="stylesheet" href="css/mant.css">
+
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/facebox.js"></script>
+	<script src="js/main.js"></script>
+	<script>
+		$(document).ready(function()
+		{
+			$('a[rel*=facebox]').facebox();
+		})
+	</script>
+
 </head>
-<header> 
-      <a href="#inicio" class="logo" data-scroll=""> <img src="resourcesGIN/imgGIN/inicio/logo-GINxti.png" height="60px" style="margin-top:10px; margin-right:20px"> </a>
-      <nav class="nav-collapse" style="padding-top:7px">
-</header>
+	
 <body>
-    
+	<div class="preloader" name="preloader"><img src="images/loading.gif"></div>
+	<header class="navbar navbar-bright navbar-fixed-top" role="banner">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" style="padding:0;"><img src="images/logo_blanco.png" alt="Universidad Simón Bolivar" height="50"></a>
+			</div>
+			<nav class="collapse navbar-collapse" role="navigation">
+			
+			
+			</nav>
+		</div>
+	</header>
+
+	
+	<section>
+		<article class="container">
+		
+				<form class="form-signin" method="post" action="" autocomplete="off" style="margin-top:3em;">
+					<div class="row">
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4">
+							<h4 class="form-signin-heading">Por favor ingrese su matrícula y contraseña.</h4>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4">
+							<label for="matricula" class="sr-only" >Matrícula</label>
+							<input type="text" name="matricula" id="matricula" class="form-control" placeholder="Matrícula" required autofocus value="">
+						</div>
+					</div>
+					<div class="row" style="margin-top:2em;">
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4">
+							<label for="passwd" class="sr-only" >Contraseña</label>
+							<input type="password" name="passwd" id="passwd" class="form-control" placeholder="Contraseña" required value="">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4">
+							<div class="checkbox">
+								<label>
+								 <a href="recupera.php"> ¿Olvido su contraseña?</a>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="row" >
+						<div class="col-sm-4"></div>
+						<div class="col-sm-4">
+							<input type="hidden" name="w" id="w" value=""><input type="hidden" name="h" id="h" value="">
+							<button class="btn btn-lg btn-primary btn-block" type="submit">Aceptar</button>
+						</div>
+					</div>
+
+				</form>
+				<script>
+					document.getElementById('w').value = screen.width;
+					document.getElementById('h').value = screen.height;
+				</script>
+			</div>
+			
+		</article>
+	</section>
+
+	<div class="prefooter">&nbsp;</div>
+	<footer class="footer visible-md visible-lg">
+		<div class="container">
+			<div style="float:left;">Per Scientiam ad Veritatem</div>
+			<div style="float:right;">viernes 13 de abril de 2018</div>
+		</div>
+	</footer>
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,113 @@
+function isValidDate(day,month,year)
+{
+	var dteDate;
+	month=month-1;
+	dteDate=new Date(year,month,day);
+	return ((day==dteDate.getDate()) && (month==dteDate.getMonth()) && (year==dteDate.getFullYear()));
+}
+
+function validate_fecha(fecha)
+{
+	var patron=new RegExp("^(19|20)+([0-9]{2})([-])([0-9]{1,2})([-])([0-9]{1,2})$");
+
+	if(fecha.search(patron)==0)
+	{
+		var values=fecha.split("-");
+		if(isValidDate(values[2],values[1],values[0]))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+function calcularEdad(f,d)
+{
+	var fecha=document.getElementById(f).value;
+	//alert(fecha);
+
+	if(validate_fecha(fecha)==true)
+	{
+		// Si la fecha es correcta, calculamos la edad
+		var values=fecha.split("-");
+		var dia = values[2];
+		var mes = values[1];
+		var ano = values[0];
+
+		// cogemos los valores actuales
+		var fecha_hoy = new Date();
+		var ahora_ano = fecha_hoy.getYear();
+		var ahora_mes = fecha_hoy.getMonth()+1;
+		var ahora_dia = fecha_hoy.getDate();
+
+		// realizamos el calculo
+		var edad = (ahora_ano + 1900) - ano;
+		if ( ahora_mes < mes )
+		{
+			edad--;
+		}
+		if ((mes == ahora_mes) && (ahora_dia < dia))
+		{
+			edad--;
+		}
+		if (edad > 1900)
+		{
+			edad -= 1900;
+		}
+
+		// calculamos los meses
+		var meses=0;
+		if(ahora_mes>mes)
+			meses=ahora_mes-mes;
+		if(ahora_mes<mes)
+			meses=12-(mes-ahora_mes);
+		if(ahora_mes==mes && dia>ahora_dia)
+			meses=11;
+
+		// calculamos los dias
+		var dias=0;
+		if(ahora_dia>dia)
+			dias=ahora_dia-dia;
+		if(ahora_dia<dia)
+		{
+			ultimoDiaMes=new Date(ahora_ano, ahora_mes, 0);
+			dias=ultimoDiaMes.getDate()-(dia-ahora_dia);
+		}
+
+		//document.getElementById("result").innerHTML="Tienes "+edad+" años, "+meses+" meses y "+dias+" días";
+		/*
+		var resultado['edad'] = edad;
+		var resultado['meses'] = meses;
+		var resultado['dias'] = dias;
+		*/
+		document.getElementById(d).value = edad;
+		//return edad;
+
+	}else
+	{
+		//return false;
+		//document.getElementById("result").innerHTML="La fecha "+fecha+" es incorrecta";
+		document.getElementById(d).value = '';
+	}
+//*/
+}
+
+function buscar_cp(cp)
+{
+	var datos = {
+				"cp": cp
+			};
+	$.ajax({
+		type: 'POST',
+		url: '00_busca_cp.php',
+		data: datos,
+		success: function(respuesta)
+		{
+			//Copiamos el resultado en #mostrar
+			r = $.parseJSON(respuesta);
+			$('#colonia').val(r.colonia);
+			$('#poblacion').val(r.ciudad);
+			$('#delegacion').val(r.delegacion);
+		}
+	});
+}
